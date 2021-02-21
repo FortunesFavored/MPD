fe5aead1d272ca5ccf7019133319da0dfc52f0e9
FortunesFavored <gregory.j.maj@gmail.com>
2021-02-21 13:26:59 -0600

    updates -GM

--- MPD.py
+++ MPD.py
@@ -1,16 +1,37 @@
-import tifffile
-import numpy
-import xarray
+import numpy as np
+import xarray as xr
 #import dbfread
-from dbfread import DBF
+import rasterio
+from affine import Affine
+# import gdal
+# from dbfread import DBF
 import math
-from tifffile import imread
+# from tifffile import imread
 ##Be better in the future
 
 # to open a tiff file for reading:
-popCount_tif = imread('C:/Users\grego\Documents\Coding Temple 2021\Moment of Population Density\World-Data\gpw-v4-population-count-rev11_2020_1_deg_tif\gpw_v4_population_count_rev11_2020_1_deg.tif', key=0)
-natID_tif = imread('C:/Users\grego\Documents\Coding Temple 2021\Moment of Population Density\World-Data\gpw-v4-national-identifier-grid-rev11_1_deg_tif\gpw_v4_national_identifier_grid_rev11_1_deg.tif', key=0)
+popCount_tif = xr.open_rasterio('C:/Users\grego\Documents\Coding Temple 2021\Moment of Population Density\World-Data\gpw-v4-population-count-rev11_2020_1_deg_tif\gpw_v4_population_count_rev11_2020_1_deg.tif')
+natID_tif = xr.open_rasterio('C:/Users\grego\Documents\Coding Temple 2021\Moment of Population Density\World-Data\gpw-v4-national-identifier-grid-rev11_1_deg_tif\gpw_v4_national_identifier_grid_rev11_1_deg.tif')
 #f = open('data.xls',"w")
+pt = popCount_tif
+y = pt.coords['y'].values
+x = pt.coords['x'].values
+# print(len(pt.coords['y'].values))
+# print(len(pt.coords['x'].values))
+# print(pt.values[0][15000][15000])
+for i in range(len(y)):
+    for j in range(len(x)):
+        if pt.values[0][i][j] > 0:
+            print('latitude:' , y[i],'longitude:',x[j],'population:',pt.values[0][i][j])
+
+# for i in x.parse_coordinates():
+#     # if i =='coords':
+#     print(i)
+
+
+# print(x)
+
+exit(0)
 
 country = {'32767':'32767'}
 total_pop = {'32767':0}
--- README.md
+++
@@ -1,2 +0,0 @@
-# MPD
-population
