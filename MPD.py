2a5119c19326993b86209e40e6998dbad286a933
FortunesFavored <gregory.j.maj@gmail.com>
2021-02-28 15:21:08 -0600

    updates -GM

--- MPD.py
+++ MPD.py
@@ -6,34 +6,14 @@ from dbfread import DBF
 import math
 import sys
 
-
-#I need proof
-
-# to open a tiff file for reading:
 pt = xr.open_rasterio('C:/Users\grego\Documents\Coding Temple 2021\Moment of Population Density\World-Data\gpw-v4-population-count-rev11_2020_30_sec_tif\gpw_v4_population_count_rev11_2020_30_sec.tif')
 nt = xr.open_rasterio('C:/Users\grego\Documents\Coding Temple 2021\Moment of Population Density\World-Data\gpw-v4-national-identifier-grid-rev11_30_sec_tif\gpw_v4_national_identifier_grid_rev11_30_sec.tif')
-#f = open('data.xls',"w")
-# print(len(pt.coords['y'].values))
-# print(len(pt.coords['x'].values))
-# print(pt.values[0][15000][15000])
-# for i in range(len(y)):
-#     for j in range(len(x)):
-        # if pt.values[0][i][j] > 0:
-        #     print('latitude:' , y[i],'longitude:',x[j],'population:',pt.values[0][i][j])
-
-# for i in x.parse_coordinates():
-#     # if i =='coords':
-#     print(i)
-
-
-# print(x)
-
 
 country = {32767:'32767',-32768:'-32768'}
 total_pop = {'32767':0,'-32768':0}
 total_vector = {'32767':[0,0,0],'-32768':[0,0,0]}
 
-for record in DBF('C:/Users\grego\Documents\Coding Temple 2021\Moment of Population Density\World-Data\gpw-v4-national-identifier-grid-rev11_1_deg_tif\gpw_v4_national_identifier_grid_rev11_1_deg.dbf'):
+for record in DBF('C:/Users\grego\Documents\Coding Temple 2021\Moment of Population Density\World-Data\gpw-v4-national-identifier-grid-rev11_30_sec_tif\gpw_v4_national_identifier_grid_rev11_30_sec.dbf'):
     country[record['Value']] = record['NAME0']
     total_pop[record['NAME0']] = 0
     total_vector[record['NAME0']] = [0 , 0, 0]
@@ -52,8 +32,6 @@ def cart_sph(x,y,z):
     phi = math.asin(z/r)
     return[theta,phi,r]
 
-
-
 for i in range(len(pt.x)):
     print(i, file=sys.stderr)
     for j in range(len(pt.y)):
@@ -71,39 +49,3 @@ for el in country.values():
     vector[0] *= 180/math.pi
     vector[1] *= 180/math.pi
     print(el,"\t",vector[0],"\t",vector[1])
-
-# print(total_vector)
-
-            # coord.append(natID_tif[j][i])
-
-        # print('latitude:' , lat_coord,'longitude:',long_coord,'population:',population, 'Contry Name', country_name)
-        # continue
-
-
-
-        
-        #     # print("lat",lat,"long", lon)
-        #     total_pop[country_name] += population
-        #     # coord = sph_cart(lon*(math.pi/180), lat*(math.pi/180))
-            # print("coord",coord)
-            # #print(coord)
-            
-            # print(coord)
-            #print(lat, lon, popCount_tif[i][j])
-            #f.write(str(lat)+','+str(lon)+','+str(popCount_tif[i][j])+','+str(natID_tif[i][j])+'\n')
-
-
-
-# print(total_pop)
-# print(total_vector)
-
-
-
-
-#print(natID_tif.shape)
-#print(popCount_tif.shape)
-#print(image1[50])
-
-#C:\Users\grego\OneDrive\Documents\Coding Temple 2021\Population Vector Density\World Data\gpw-v4-national-identifier-grid-rev11_1_deg_tif
-#Population Vector Density\World Data\gpw-v4-national-identifier-grid-rev11_1_deg_tif
-#[MSC v.1916 64 bit (AMD64)]
--- test.py
+++ test.py
@@ -1,3 +1,16 @@
-import sys
-print('it worked')
-print("fatal error", file=sys.stderr)
+import re
+import json
+
+
+arr = []
+y = open('data_output.txt', "w")
+
+for i in open('data_copy.txt').readlines():
+    print(i)
+    x = list(re.findall('(\D+) (\-?\d+\.\d+)\s+(\-?\d+\.\d+)',i)[0])
+    x[0] = x[0].strip()
+    x[1],x[2] = float(x[1]),float(x[2])
+    arr.append(x)
+
+y.write("popData = "+json.dumps(arr))
+
